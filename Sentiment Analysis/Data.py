c5250777db4d490ddcd3cfb54c135ca38cfd4a19
wonedav <wonderdavid2@gmail.com>
2022-07-05 08:25:02 +0100

    started project

--- Sentiment Analysis/Data.py
+++ Sentiment Analysis/Data.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 class Data:
     def __init__(self):
-        self.data = pd.read_csv("review.csv", parse_dates=['reviewTime'])
+        data = pd.read_csv("review.csv", parse_dates=['reviewTime'])
+        self.data = data[data['reviewText'].notnull()]
     def get_data(self):
         return self.data
     def get_column(self, column):
